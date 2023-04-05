d542900f8a6c4d08a5a1592d7c7fcdad1fa2752b
Felipe Flores Valdivia <felipe.floresv@alumnos.usm.cl>
2023-04-05 11:15:50 -0400

    Complete challenge number 3 and add new library to use XGBOOST in requirements. In addition, new utils functions was added

--- utils/pddataframes.py
+++ utils/pddataframes.py
@@ -9,7 +9,7 @@ def print_ocurrencies(dataframe, columns, text="Ocurrencies for {} column"):
         print("\n")
 
 
-def plot_barchar(value_count_dict, column):
+def plot_barchar(value_count_dict, column, label="{} distribution"):
 
     column_values = list(value_count_dict.keys())
 
@@ -31,14 +31,36 @@ def plot_barchar(value_count_dict, column):
     ax.barh(y_pos, ocurrencies, align = 'center')
     ax.set_yticks(y_pos, labels = column_values)
     ax.invert_yaxis()  # labels read top-to-bottom\
-    ax.set_title('{} distribution'.format(column)) 
+    ax.set_title(label.format(column)) 
+
+def plot_grouped_barchar(first_dataframe, second_dataframe, columns):
+    for column in columns:
+        fdf_values = first_dataframe[column].value_counts().to_dict()
+        sdf_values = second_dataframe[column].value_counts().to_dict()
+        all_labels = list(set(fdf_values.keys()).union( set(sdf_values.keys())))
+        x = np.arange(len(all_labels))
+        y1 = fdf_values.values()
+        y2 = sdf_values.values()
+        width = 0.40
+
+        plt.bar(x-0.2, y1, width)
+        plt.bar(x+0.2, y2, width)
+        plt.xticks(x, all_labels)
+        plt.legend(["delayed", "in time"])
+        plt.title("{} delayed compared".format(column))
+        plt.show()
 
 
-def plot_multiple_barchar(dataframe, columns):
+def plot_grouped_barchar_withpd(df, columns):
+    df_gb = df.groupby(columns).size().unstack(level=1)
+    df_gb.plot(kind = 'bar')
+
+
+def plot_multiple_barchar(dataframe, columns, label="{} distribution"):
     for column in columns:
         value_count_dict = dataframe[column].value_counts().to_dict()
 
-        plot_barchar(value_count_dict, column)
+        plot_barchar(value_count_dict, column, label)
     plt.show()
 
 
@@ -85,3 +107,16 @@ def compare_times_of_day(string_dt):
         return "morning"
     else:
         return "afternoon"
+
+def get_ratios(delayed_flights, in_time_flights, columns):
+    all_ratios = {}
+    for column in columns:
+        
+        dly_flight_values = delayed_flights[[column]].value_counts().to_dict()
+        int_flight_values = in_time_flights[[column]].value_counts().to_dict() 
+        ratio_dict = {}
+        for key, value in dly_flight_values.items():
+            key_val = key[0] if isinstance(key, tuple) else key
+            ratio_dict[key_val] =  value / (value + int_flight_values[key]) if key in int_flight_values else 1
+        all_ratios[column] = ratio_dict
+    return all_ratios
